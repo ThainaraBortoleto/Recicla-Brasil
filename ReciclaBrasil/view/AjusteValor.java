c6ed2663a3cddd2980db897be5ad1348284be049
arthurgrigoletto <arthur.grigoletto.lima@gmail.com>
2017-10-31 13:26:48 -0200

    Observador + Ajustes de Valores

---
+++ ReciclaBrasil/view/AjusteValor.java
@@ -0,0 +1,229 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AjusteValor extends JFrame{
+	private JPanel contentPane, panelAjuste;
+	private JMenuBar menuBar;
+	private JMenu menuHome, menuColetor, menuEmpresa, menuAjustarValores, menuSair;
+	private JMenuItem pagInicial, cadastrarColetor, consultarColetor, cadastrarEmpresa, consultarEmpresa, ajusteValor, sair;
+	private JPanel panelBody, panelRodape;
+	private JLabel lblAjustarValores, lblPainelDeControle, lblMateriais, lblAluminio, lblPapelao, lblPlastico, lblVidro, lblPrecoAluminio,  lblPreco, lblMaterial, lblPrecoPapelao, lblPrecoPlastico, lblPrecoVidro, lblCopyRight, lblDesenvolvedores;
+	private JButton btnEditarAluminio, btnEditarPapelao, btnEditarPlastico, btnEditarVidro;
+	private Observador observador;
+	
+	public AjusteValor() {
+		super("ReciclaBrasil");
+
+		menuBar = new JMenuBar();
+		menuBar.setBackground(Color.LIGHT_GRAY);
+		setJMenuBar(menuBar);
+		
+		menuHome = new JMenu("Home");
+		menuBar.add(menuHome);
+		
+		pagInicial = new JMenuItem("Página Inicial");
+		menuHome.add(pagInicial);
+
+		menuColetor = new JMenu("Coletor");
+		menuBar.add(menuColetor);
+
+		cadastrarColetor = new JMenuItem("Cadastrar");
+		menuColetor.add(cadastrarColetor);
+
+		consultarColetor = new JMenuItem("Consultar");
+		menuColetor.add(consultarColetor);
+
+		menuEmpresa = new JMenu("Empresa");
+		menuBar.add(menuEmpresa);
+
+		cadastrarEmpresa = new JMenuItem("Cadastrar");
+		menuEmpresa.add(cadastrarEmpresa);
+
+		consultarEmpresa = new JMenuItem("Consultar");
+		menuEmpresa.add(consultarEmpresa);
+
+		menuAjustarValores = new JMenu("Ajustar Valores");
+		menuBar.add(menuAjustarValores);
+		
+		ajusteValor = new JMenuItem("Ajustar valores");
+		menuAjustarValores.add(ajusteValor);
+
+		menuSair = new JMenu("Sair");
+		menuBar.add(menuSair);
+		
+		sair = new JMenuItem("Sair");
+		menuSair.add(sair);
+
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		panelBody = new JPanel();
+		panelBody.setBackground(Color.WHITE);
+		contentPane.add(panelBody, BorderLayout.CENTER);
+		panelBody.setLayout(null);
+
+		// Titulo
+		lblAjustarValores = new JLabel("Ajustar Valores");
+		lblAjustarValores.setForeground(Color.DARK_GRAY);
+		lblAjustarValores.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		lblAjustarValores.setBounds(16, 6, 148, 25);
+		panelBody.add(lblAjustarValores);
+		
+		lblPainelDeControle = new JLabel("Painel de Controle");
+		lblPainelDeControle.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		lblPainelDeControle.setForeground(Color.LIGHT_GRAY);
+		lblPainelDeControle.setBounds(163, 12, 111, 16);
+		panelBody.add(lblPainelDeControle);
+
+		//Conteúdo
+		panelAjuste = new JPanel();
+		panelAjuste.setBounds(60, 168, 1026, 336);
+		panelBody.add(panelAjuste);
+		panelAjuste.setLayout(null);
+		
+		lblMateriais = new JLabel("Materiais");
+		lblMateriais.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		lblMateriais.setBounds(6, 6, 94, 16);
+		panelAjuste.add(lblMateriais);
+		
+		lblAluminio = new JLabel("Alumínio");
+		lblAluminio.setBounds(6, 72, 61, 16);
+		panelAjuste.add(lblAluminio);
+		
+		lblPapelao = new JLabel("Papelão");
+		lblPapelao.setBounds(6, 114, 61, 16);
+		panelAjuste.add(lblPapelao);
+		
+		lblPlastico = new JLabel("Plástico");
+		lblPlastico.setBounds(6, 161, 61, 16);
+		panelAjuste.add(lblPlastico);
+		
+		lblVidro = new JLabel("Vidro");
+		lblVidro.setBounds(6, 207, 61, 16);
+		panelAjuste.add(lblVidro);
+		
+		lblPrecoAluminio = new JLabel("3.50");
+		lblPrecoAluminio.setBounds(320, 72, 61, 16);
+		panelAjuste.add(lblPrecoAluminio);
+		
+		lblPreco = new JLabel("Preço");
+		lblPreco.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		lblPreco.setBounds(320, 44, 61, 16);
+		panelAjuste.add(lblPreco);
+		
+		lblMaterial = new JLabel("Material");
+		lblMaterial.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		lblMaterial.setBounds(6, 44, 61, 16);
+		panelAjuste.add(lblMaterial);
+		
+		lblPrecoPapelao = new JLabel("3.00");
+		lblPrecoPapelao.setBounds(320, 114, 61, 16);
+		panelAjuste.add(lblPrecoPapelao);
+		
+		lblPrecoPlastico = new JLabel("2.50");
+		lblPrecoPlastico.setBounds(320, 161, 61, 16);
+		panelAjuste.add(lblPrecoPlastico);
+		
+		lblPrecoVidro = new JLabel("4.50");
+		lblPrecoVidro.setBounds(320, 207, 61, 16);
+		panelAjuste.add(lblPrecoVidro);
+		
+		btnEditarAluminio = new JButton("Editar");
+		btnEditarAluminio.setBounds(669, 67, 117, 29);
+		panelAjuste.add(btnEditarAluminio);
+		
+		btnEditarPapelao = new JButton("Editar");
+		btnEditarPapelao.setBounds(669, 109, 117, 29);
+		panelAjuste.add(btnEditarPapelao);
+		
+		btnEditarPlastico = new JButton("Editar");
+		btnEditarPlastico.setBounds(669, 156, 117, 29);
+		panelAjuste.add(btnEditarPlastico);
+		
+		btnEditarVidro = new JButton("Editar");
+		btnEditarVidro.setBounds(669, 202, 117, 29);
+		panelAjuste.add(btnEditarVidro);
+		
+
+		// Rodapé
+		panelRodape = new JPanel();
+		panelRodape.setBackground(Color.LIGHT_GRAY);
+		contentPane.add(panelRodape, BorderLayout.SOUTH);
+
+		lblCopyRight = new JLabel("© Copyright 2001-2017");
+		lblCopyRight.setForeground(Color.WHITE);
+		panelRodape.add(lblCopyRight);
+
+		lblDesenvolvedores = new JLabel(
+				"André Gianfratti, Arthur Grigoletto, Guilherme Araújo, Raul Machado, Thainara Bortoleto");
+		lblDesenvolvedores.setForeground(Color.WHITE);
+		lblDesenvolvedores.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		panelRodape.add(lblDesenvolvedores);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1161, 808);
+		setVisible(true);
+
+		// observador
+		observador = new Observador();
+		//MenuBar
+		pagInicial.addActionListener(observador);
+		cadastrarColetor.addActionListener(observador);
+		consultarColetor.addActionListener(observador);
+		cadastrarEmpresa.addActionListener(observador);
+		consultarEmpresa.addActionListener(observador);
+		ajusteValor.addActionListener(observador);
+		sair.addActionListener(observador);
+		//Editar
+		btnEditarAluminio.addActionListener(observador);
+		btnEditarPapelao.addActionListener(observador);
+		btnEditarPlastico.addActionListener(observador);
+		btnEditarVidro.addActionListener(observador);
+		
+
+	}
+
+	class Observador implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == pagInicial) {
+				new Home();
+			}else if (e.getSource() == cadastrarColetor) {
+				System.out.println("Cadastrar coletor");
+			} else if (e.getSource() == consultarColetor) {
+				System.out.println("Consultar coletor");
+			} else if (e.getSource() == cadastrarEmpresa) {
+				System.out.println("Cadastrar Empresa");
+			} else if (e.getSource() == consultarEmpresa) {
+				System.out.println("Consultar Empresa");
+			} else if (e.getSource() == ajusteValor) {
+				new AjusteValor();
+			} else if(e.getSource() == btnEditarAluminio) {
+				lblPrecoAluminio.setText(JOptionPane.showInputDialog("Novo valor Alumínio: "));
+			} else if(e.getSource() == btnEditarPapelao) {
+				lblPrecoPapelao.setText(JOptionPane.showInputDialog("Novo valor Alumínio: "));
+			} else if(e.getSource() == btnEditarPlastico) {
+				lblPrecoPlastico.setText(JOptionPane.showInputDialog("Novo valor Alumínio: "));
+			} else if(e.getSource() == btnEditarVidro) {
+				lblPrecoVidro.setText(JOptionPane.showInputDialog("Novo valor Alumínio: "));
+			} else {
+				System.exit(0);
+			}
+		}
+	}
+}
--- ReciclaBrasil/view/Home.java
+++ ReciclaBrasil/view/Home.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,205 +10,222 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.border.EmptyBorder;
 
-public class Home extends JFrame{
+public class Home extends JFrame {
 	private JPanel contentPane;
-	JMenuBar menuBar;
-	JMenu menuColetor, menuEmpresa, menuAjustarValores, menuSair;
-	JMenuItem cadastrarColetor, consultarColetor, cadastrarEmpresa, consultarEmpresa;
-	JPanel panelBody, panelRodape;
-	JLabel lblAluminio, lblPapelao, lblPlastico, lblVidro, lblTotalDeMaterial, lblIncio, lblPainelDeControle, lblAgenda, porcentagemAluminio, porcentagemPapelao, porcentagemPlastico, 
-		porcentagemVidro, lblNoveHoras, lblDezHoras, lblOnzeHoras, lblDozeHoras, lblTrezeHoras, lblQuatorzeHoras, lblQuinzeHoras, lblDezesseisHoras, lblDezeseteHoras, lblDezoitoHoras, lblEmpresaX, 
-			lblEmpresaY, lblEmpresaXy, lblEmpresaK, lblCopyRight, lblDesenvolvedores;
-	
+	private JMenuBar menuBar;
+	private JMenu menuHome, menuColetor, menuEmpresa, menuAjustarValores, menuSair;
+	private JMenuItem pagInicial, cadastrarColetor, consultarColetor, cadastrarEmpresa, consultarEmpresa, ajusteValor, sair;
+	private JPanel panelBody, panelRodape;
+	private JLabel lblAluminio, lblPapelao, lblPlastico, lblVidro, lblTotalDeMaterial, lblIncio, lblPainelDeControle, lblAgenda,
+			porcentagemAluminio, porcentagemPapelao, porcentagemPlastico, porcentagemVidro, lblNoveHoras, lblDezHoras,
+			lblOnzeHoras, lblDozeHoras, lblTrezeHoras, lblQuatorzeHoras, lblQuinzeHoras, lblDezesseisHoras,
+			lblDezeseteHoras, lblDezoitoHoras, lblEmpresaX, lblEmpresaY, lblEmpresaXy, lblEmpresaK, lblCopyRight,
+			lblDesenvolvedores;
+	private Observador observador;
+
 	public Home() {
 		super("ReciclaBrasil");
-		
+
 		menuBar = new JMenuBar();
 		menuBar.setBackground(Color.LIGHT_GRAY);
 		setJMenuBar(menuBar);
 		
+		menuHome = new JMenu("Home");
+		menuBar.add(menuHome);
+		
+		pagInicial = new JMenuItem("Página Inicial");
+		menuHome.add(pagInicial);
+
 		menuColetor = new JMenu("Coletor");
 		menuBar.add(menuColetor);
-		
+
 		cadastrarColetor = new JMenuItem("Cadastrar");
 		menuColetor.add(cadastrarColetor);
-		
+
 		consultarColetor = new JMenuItem("Consultar");
 		menuColetor.add(consultarColetor);
-		
-		 menuEmpresa = new JMenu("Empresa");
+
+		menuEmpresa = new JMenu("Empresa");
 		menuBar.add(menuEmpresa);
-		
+
 		cadastrarEmpresa = new JMenuItem("Cadastrar");
 		menuEmpresa.add(cadastrarEmpresa);
-		
+
 		consultarEmpresa = new JMenuItem("Consultar");
 		menuEmpresa.add(consultarEmpresa);
-		
+
 		menuAjustarValores = new JMenu("Ajustar Valores");
 		menuBar.add(menuAjustarValores);
 		
+		ajusteValor = new JMenuItem("Ajustar valores");
+		menuAjustarValores.add(ajusteValor);
+
 		menuSair = new JMenu("Sair");
 		menuBar.add(menuSair);
 		
+		sair = new JMenuItem("Sair");
+		menuSair.add(sair);
+
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		panelBody = new JPanel();
 		panelBody.setBackground(Color.WHITE);
 		contentPane.add(panelBody, BorderLayout.CENTER);
 		panelBody.setLayout(null);
-		
-		//Titulo
+
+		// Titulo
 		lblIncio = new JLabel("Início");
 		lblIncio.setForeground(Color.DARK_GRAY);
 		lblIncio.setFont(new Font("Lucida Grande", Font.BOLD, 18));
 		lblIncio.setBounds(16, 6, 61, 25);
 		panelBody.add(lblIncio);
-		
+
 		lblPainelDeControle = new JLabel("Painel de Controle");
 		lblPainelDeControle.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
 		lblPainelDeControle.setForeground(Color.LIGHT_GRAY);
 		lblPainelDeControle.setBounds(75, 12, 111, 16);
 		panelBody.add(lblPainelDeControle);
-		
-		//ProgressBar
+
+		// ProgressBar
 		lblTotalDeMaterial = new JLabel("Total de Material");
 		lblTotalDeMaterial.setFont(new Font("Lucida Grande", Font.BOLD, 20));
 		lblTotalDeMaterial.setBounds(457, 82, 186, 25);
 		panelBody.add(lblTotalDeMaterial);
-		
+
 		JProgressBar progressBarAluminio = new JProgressBar();
 		progressBarAluminio.setValue(55);
 		progressBarAluminio.setBounds(284, 133, 579, 20);
 		panelBody.add(progressBarAluminio);
-		
+
 		lblAluminio = new JLabel("Alumínio");
 		lblAluminio.setBounds(154, 133, 61, 16);
 		panelBody.add(lblAluminio);
-		
+
 		lblPapelao = new JLabel("Papelão");
 		lblPapelao.setBounds(154, 161, 61, 16);
 		panelBody.add(lblPapelao);
-		
+
 		lblPlastico = new JLabel("Plástico");
 		lblPlastico.setBounds(154, 189, 61, 16);
 		panelBody.add(lblPlastico);
-		
+
 		lblVidro = new JLabel("Vidro");
 		lblVidro.setBounds(154, 217, 61, 16);
 		panelBody.add(lblVidro);
-		
+
 		JProgressBar progressBarPapelao = new JProgressBar();
 		progressBarPapelao.setValue(70);
 		progressBarPapelao.setBounds(284, 157, 579, 20);
 		panelBody.add(progressBarPapelao);
-		
+
 		JProgressBar progressBarPlastico = new JProgressBar();
 		progressBarPlastico.setValue(30);
 		progressBarPlastico.setBounds(284, 185, 579, 20);
 		panelBody.add(progressBarPlastico);
-		
+
 		JProgressBar progressBarVidro = new JProgressBar();
 		progressBarVidro.setValue(90);
 		progressBarVidro.setBounds(284, 213, 579, 20);
 		panelBody.add(progressBarVidro);
-		
+
 		porcentagemAluminio = new JLabel("55%");
 		porcentagemAluminio.setBounds(916, 133, 61, 16);
 		panelBody.add(porcentagemAluminio);
-		
+
 		porcentagemPapelao = new JLabel("70%");
 		porcentagemPapelao.setBounds(916, 161, 61, 16);
 		panelBody.add(porcentagemPapelao);
-		
+
 		porcentagemPlastico = new JLabel("30%");
 		porcentagemPlastico.setBounds(916, 189, 61, 16);
 		panelBody.add(porcentagemPlastico);
-		
+
 		porcentagemVidro = new JLabel("90%");
 		porcentagemVidro.setBounds(916, 217, 61, 16);
 		panelBody.add(porcentagemVidro);
-		
-		//Agenda
+
+		// Agenda
 		lblAgenda = new JLabel("Agenda");
 		lblAgenda.setFont(new Font("Lucida Grande", Font.BOLD, 20));
 		lblAgenda.setBounds(511, 322, 89, 34);
 		panelBody.add(lblAgenda);
-		
+
 		lblNoveHoras = new JLabel("09:00");
 		lblNoveHoras.setBounds(154, 369, 61, 16);
 		panelBody.add(lblNoveHoras);
-		
+
 		lblDezHoras = new JLabel("10:00");
 		lblDezHoras.setBounds(154, 400, 61, 16);
 		panelBody.add(lblDezHoras);
-		
+
 		lblOnzeHoras = new JLabel("11:00");
 		lblOnzeHoras.setBounds(154, 428, 61, 16);
 		panelBody.add(lblOnzeHoras);
-		
+
 		lblDozeHoras = new JLabel("12:00");
 		lblDozeHoras.setBounds(154, 456, 61, 16);
 		panelBody.add(lblDozeHoras);
-		
+
 		lblTrezeHoras = new JLabel("13:00");
 		lblTrezeHoras.setBounds(154, 486, 61, 16);
 		panelBody.add(lblTrezeHoras);
-		
+
 		lblQuatorzeHoras = new JLabel("14:00");
 		lblQuatorzeHoras.setBounds(154, 514, 61, 16);
 		panelBody.add(lblQuatorzeHoras);
-		
+
 		lblQuinzeHoras = new JLabel("15:00");
 		lblQuinzeHoras.setBounds(154, 542, 61, 16);
 		panelBody.add(lblQuinzeHoras);
-		
+
 		lblDezesseisHoras = new JLabel("16:00");
 		lblDezesseisHoras.setBounds(154, 570, 61, 16);
 		panelBody.add(lblDezesseisHoras);
-		
+
 		lblDezeseteHoras = new JLabel("17:00");
 		lblDezeseteHoras.setBounds(154, 598, 61, 16);
 		panelBody.add(lblDezeseteHoras);
-		
+
 		lblDezoitoHoras = new JLabel("18:00");
 		lblDezoitoHoras.setBounds(154, 626, 61, 16);
 		panelBody.add(lblDezoitoHoras);
-		
+
 		lblEmpresaX = new JLabel("Empresa X");
 		lblEmpresaX.setBounds(284, 369, 81, 16);
 		panelBody.add(lblEmpresaX);
-		
+
 		lblEmpresaY = new JLabel("Empresa Y");
 		lblEmpresaY.setBounds(284, 400, 70, 16);
 		panelBody.add(lblEmpresaY);
-		
+
 		lblEmpresaXy = new JLabel("Empresa XY");
 		lblEmpresaXy.setBounds(284, 456, 81, 16);
 		panelBody.add(lblEmpresaXy);
-		
+
 		lblEmpresaK = new JLabel("Empresa K");
 		lblEmpresaK.setBounds(284, 598, 81, 16);
 		panelBody.add(lblEmpresaK);
-		
-		//Rodapé
+
+		// Rodapé
 		panelRodape = new JPanel();
 		panelRodape.setBackground(Color.LIGHT_GRAY);
 		contentPane.add(panelRodape, BorderLayout.SOUTH);
-		
+
 		lblCopyRight = new JLabel("© Copyright 2001-2017");
 		lblCopyRight.setForeground(Color.WHITE);
 		panelRodape.add(lblCopyRight);
-		
-		lblDesenvolvedores = new JLabel("André Gianfratti, Arthur Grigoletto, Guilherme Araújo, Raul Machado, Thainara Bortoleto");
+
+		lblDesenvolvedores = new JLabel(
+				"André Gianfratti, Arthur Grigoletto, Guilherme Araújo, Raul Machado, Thainara Bortoleto");
 		lblDesenvolvedores.setForeground(Color.WHITE);
 		lblDesenvolvedores.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
 		panelRodape.add(lblDesenvolvedores);
@@ -214,7 +233,36 @@ public class Home extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1161, 808);
 		setVisible(true);
-		
-		
+
+		// observador
+		observador = new Observador();
+		pagInicial.addActionListener(observador);
+		cadastrarColetor.addActionListener(observador);
+		consultarColetor.addActionListener(observador);
+		cadastrarEmpresa.addActionListener(observador);
+		consultarEmpresa.addActionListener(observador);
+		ajusteValor.addActionListener(observador);
+		sair.addActionListener(observador);
+
+	}
+
+	class Observador implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == pagInicial) {
+				new Home();
+			} else if (e.getSource() == cadastrarColetor) {
+				System.out.println("Cadastrar coletor");
+			} else if (e.getSource() == consultarColetor) {
+				System.out.println("Consultar coletor");
+			} else if (e.getSource() == cadastrarEmpresa) {
+				System.out.println("Cadastrar Empresa");
+			} else if (e.getSource() == consultarEmpresa) {
+				System.out.println("Consultar Empresa");
+			} else if (e.getSource() == ajusteValor) {
+				new AjusteValor();
+			} else {
+				System.exit(0);
+			}
+		}
 	}
 }
